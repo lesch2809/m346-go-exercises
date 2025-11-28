83d180b55c708becc389f08749cf68a6af6ffd5d
Levin Schmid <levin.schmid@mein.gmx>
2025-11-28 14:31:47 +0100

    exe_1_1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName, lastName, dayOfBirth, monthOfBirth, yearOfBirth, numberOfSiblings, heightInMeters, zodiacSign = "Levin", "schmid", 28, 04, 9, 2, 2.90, '\u2649' 
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
