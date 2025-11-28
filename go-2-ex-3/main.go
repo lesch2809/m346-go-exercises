897ed0a67d124a989b5ee2d3def3598ffe0fa3d6
Levin Schmid <levin.schmid@mein.gmx>
2025-11-28 14:41:22 +0100

    exe_2_3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[uint]string{
+		104: "das eine modul",
+		117: "das andere Modul",
+		346: "das letze modul",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
+	// TODO: delete 
+	delete(modules, 104)
 	// TODO: add one
+	modules[187] = "uk modul"
 	// TODO: replace one
+	modules[117] = "das neue modul"
 	fmt.Println(modules)
 }
