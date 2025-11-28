0a701fc489c3d85067509cfb878de97e68764f7d
Levin Schmid <levin.schmid@mein.gmx>
2025-11-28 15:12:32 +0100

    exe_2_4_v2

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -13,33 +13,45 @@ func main() {
 		students []student
 	}
 	// TODO: declare a map of modules being attended by multiple classes
-	modules := map[string]class{
-		"114": {
+	modules := map[string][]class{
+		"114": []class{
+			{
 			students: []student{
 				{firstname: "Max", lastname: "Mustermann"},
 				{firstname: "Erika", lastname: "Musterfrau"},
 				{firstname: "Julia", lastname: "Mayer"},
 			},
+			},
+			{
+			students: []student{
+				{firstname: "Tom", lastname: "Hoffmann"},
+				{firstname: "Eva", lastname: "Schulz"},
+				{firstname: "Mia", lastname: "Klein"},
+			},
 		},
-		"117": {
+		
+		},
+		"117": []class{
+			{
 			students: []student{
 				{firstname: "Hans", lastname: "Schmidt"},
 				{firstname: "Anna", lastname: "Schneider"},
 				{firstname: "Lena", lastname: "Fischer"},
 			},
 		},
-		"120": {
+			
+		},
+		"120": []class{
+			{
 			students: []student{
 				{firstname: "Peter", lastname: "Fischer"},
 				{firstname: "Laura", lastname: "Weber"},
 				{firstname: "Sophie", lastname: "Becker"},
 			},
 		},
+		},
 	}
 	// @Patrick Bucher: Wie kann ich meherer klassen zu eine kurs hinzufuegen?
 	// TODO: output everything using fmt.Println()
-	{
-
-	}
 	fmt.Println(modules)
 }
