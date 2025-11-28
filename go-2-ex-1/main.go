9600e36e411eed9c0bdb6ce1d30644591c9a949a
Levin Schmid <levin.schmid@mein.gmx>
2025-11-28 14:32:20 +0100

    exe_2_1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	firstName string
+	lastName  string
 }
 
 // TODO: declare a structure for birth date
+type Birthdate struct{
+	day		int
+	month	int
+	year	int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	birthdate 		 Birthdate
+	fullName 		 FullName
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		birthdate: Birthdate{
+			day: 1,
+			month: 1,
+			year: 2000,
+		},
+		fullName: FullName{
+			firstName: "Levin",
+			lastName: "Schmid",
+		},
+		NumberOfSiblings: 2,   // TODO: adjust
+		ZodiacSign:       'S', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings = me.NumberOfSiblings+1
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
