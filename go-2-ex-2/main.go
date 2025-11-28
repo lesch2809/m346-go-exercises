0b1b297172648eba0a05c7397238346a43b4db66
Levin Schmid <levin.schmid@mein.gmx>
2025-11-28 14:41:12 +0100

    exe_2_2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -6,9 +6,15 @@ func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
 	fibs[2] = fibs[0] + fibs[1]
+	fibs[3] = fibs[1] + fibs[2]
+	fibs[4] = fibs[2] + fibs[3]
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, 8)
+	fibs = append(fibs, 13)
+	fibs = append(fibs, 21)
+	fibs = append(fibs, 34)
+	 // TODO: replace 0 with the next Fibonacci number
 	// TODO: compute three more Fibonacci numbers and append them
 
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
