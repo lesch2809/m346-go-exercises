5966c85c30aa7e1503c51bfeaf61295d65569a56
Levin Schmid <levin.schmid@mein.gmx>
2025-12-11 20:33:41 +0100

    exe 3.3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -2,9 +2,20 @@ package main
 
 const (
 	Lower = 1
-	Upper = 30
+	Upper = 300
 )
 
 func main() {
 	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i < Upper; i++ {
+		if i%5==0 && i%3==0{
+			println("FizzBuzz")
+		}else if i%5==0{
+			println("Buzz")
+		}else if i%3==0{
+			println("Fizz")
+		}else{
+			println(i)
+		}
+	}
 }
